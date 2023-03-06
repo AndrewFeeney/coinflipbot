ac61f023f65c7eba15e752e6dbc77314cf6c2425
Andrew Feeney <andrew@deskspacecreative.com.au>
2023-03-06 11:43:02 +1100

    Starting fiddling with data structures for the domain

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,35 @@ fn main() {
     }
 }
 
+struct SocialMediaPost {
+    text: String,
+}
+struct SocialMediaPostResult {
+    success: bool,
+    post: SocialMediaPost,
+}
+
+struct TestMastodonAccount {
+    posts: Vec<String>,
+}
+
+trait Postable {
+    fn post(&mut self, text: String) -> SocialMediaPostResult;
+}
+
+impl Postable for TestMastodonAccount {
+    fn post(&mut self, text: String) -> SocialMediaPostResult {
+        self.posts.push(text.clone());
+
+        SocialMediaPostResult {
+            success: true,
+            post: SocialMediaPost {
+                text,
+            }
+        }
+    }
+}
+
 #[derive(PartialEq)]
 enum CoinFlipResult {
     Heads,
@@ -30,4 +59,17 @@ fn test_flip_coin() {
     let result = flip_coin();
 
     assert!(result == CoinFlipResult::Heads || result == CoinFlipResult::Tails);
+}
+
+#[test]
+fn test_post() {
+    let mut test_account = TestMastodonAccount {
+        posts: Vec::new(),
+    };
+
+    let text = "Hello, world!".to_string();
+    let post_result = test_account.post(text);
+
+    assert!(post_result.success == true);
+    assert!(post_result.post.text == "Hello, world!");
 }
