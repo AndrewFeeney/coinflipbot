ef4fbb44507385b4a462bb24db3f3477ed45d987
Andrew Feeney <andrew@deskspacecreative.com.au>
2023-03-16 22:41:53 +1100

    Add function for POST requests

--- src/http.rs
+++ src/http.rs
@@ -7,10 +7,22 @@ async fn get(url: &str) -> Result<String, Box<dyn std::error::Error>> {
     Ok(body)
 }
 
+async fn post(url: &str, body: String) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let response = client.post(url)
+        .body(body)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(response)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use mockito::{mock};
+    use mockito::mock;
 
     #[tokio::test]
     async fn test_get() {
@@ -22,6 +34,22 @@ mod tests {
             .create();
 
         let response = get(&format!("{}/test", mockito::server_url())).await.unwrap();
+        assert_eq!(response, mock_response);
+
+        mock_server.assert();
+    }
+
+    #[tokio::test]
+    async fn test_post() {
+        let mock_response = "Hello, world!";
+        let mock_server = mock("POST", "/test")
+            .with_status(200)
+            .with_header("content-type", "text/plain")
+            .with_body(mock_response)
+            .create();
+        let test_body = "test_body".to_owned();
+
+        let response = post(&format!("{}/test", mockito::server_url()), test_body).await.unwrap();
         assert_eq!(response, mock_response);
 
         mock_server.assert();
